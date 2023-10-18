c86c3a6fa5587888638a5375d8aa9ba16fa61755
k1ng606 <allenhartley@proton.me>
2023-10-18 10:54:12 +0200

    first iteration of enemy spawn on timer

--- scripts/enemy.gd
+++ scripts/enemy.gd
@@ -1,31 +1,39 @@
 extends CharacterBody2D
 
 @onready var targetPosition : Vector2 = $"../EnemyTarget".position
-
 @onready var enemySpawnPosition : Vector2 = $"../EnemySpawn".position
+@onready var navigation_agent := $NavigationAgent2D
 
-@onready var navigationAgent := $NavigationAgent2D
-
-const speed := 5000
+const speed := 200
 
 func _ready() -> void:
+	
+	navigation_agent.path_desired_distance = 4.0
+	navigation_agent.target_desired_distance = 4.0
+	
 	position = enemySpawnPosition
-	navigationAgent.target_position = targetPosition
 
+	# Make sure to not await during _ready.
+	call_deferred("actor_setup")
+
+
+func actor_setup()-> void:
+	await get_tree().physics_frame
+	set_movement_target(targetPosition)
+	
+func set_movement_target(movement_target: Vector2):
+	navigation_agent.target_position = movement_target
 
 func _physics_process(_delta: float) -> void:
-	await get_tree().physics_frame
-	var direction := to_local(navigationAgent.get_next_path_position()).normalized()
-	
-	velocity = direction * speed * _delta
-	
+	if navigation_agent.is_navigation_finished():
+		self.queue_free()
+
+	var current_agent_position: Vector2 = global_position
+	var next_path_position: Vector2 = navigation_agent.get_next_path_position()
+
+	var new_velocity: Vector2 = next_path_position - current_agent_position
+	new_velocity = new_velocity.normalized()
+	new_velocity = new_velocity * speed
+
+	velocity = new_velocity
 	move_and_slide()
-	
-	#if (position == targetPosition):
-		
-
-
-
-
-func _on_navigation_agent_2d_target_reached():
-	self.queue_free()
--- scripts/level.gd
+++ scripts/level.gd
@@ -3,9 +3,14 @@ extends Node2D
 @onready var enemy:PackedScene = preload("res://scenes/enemy.tscn")
 
 
-func SpawnEnemies() -> void:
-	for i in 1:
+func SpawnEnemies(number_of_enemies_to_spawn:int = 1):
+	for i in number_of_enemies_to_spawn:
 		add_child(enemy.instantiate())
 
 func _ready()-> void:
 	SpawnEnemies()
+	timerSetup()
+	
+	
+func timerSetup() -> void:
+	get_tree().create_timer(3).timeout.connect(func() : SpawnEnemies())
